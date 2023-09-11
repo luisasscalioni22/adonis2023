c5869e200a78d4764402a9c4ef33e708ede9cde1
luisasscalioni22 <luisasousascalioni@gmail.com>
2023-09-11 12:19:41 -0300

    1109

---
+++ database/migrations/1694442531185_add_column_token_to_users.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up () {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('token')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('token')
+    })
+  }
+}
